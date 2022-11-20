d6d4a64fe81ad4792743328f6c5d0469639b1c9b
KittyChen913 <nightmare.kitty111@gmail.com>
2022-11-20 17:57:17 +0800

    ⚡️ 改善 #5 的暴力破解版處理邏輯
    減少了一個 List 與 一個 for, 雖然仍然 timeout

--- LeetcodeProject/LongestPalindromicSubstring.cs
+++ LeetcodeProject/LongestPalindromicSubstring.cs
@@ -22,25 +22,20 @@ public class LongestPalindromicSubstring
     /// <returns></returns>
     public static string LongestPalindrome_BruteForceAttack(string s)
     {
-        List<string> allOptions = new();
-        var longest = string.Empty; // 用於存放最後的最長字串
+        var longest = string.Empty; // 用於存放最後的最長回文字串
 
         for (int i = 0; i < s.Length; i++)
         {
             for (int j = 1; j <= s.Length - i; j++)
             {
-                allOptions.Add(s.Substring(i, j)); // 將各種可能的 string 組合都存成 List item
-            }
-        }
-
-        // 將 List 內的 string 組合，一個一個拿出來判定，string item 反轉後是否也與原 string 相同
-        foreach (var item in allOptions)
-        {
-            var charArray = item.ToCharArray();
-            Array.Reverse(charArray);
-            if (new string(charArray) == item)
-            {
-                if (longest.Length < item.Length) longest = item; // 更長的 string 就指定至 longest
+                // 將各種可能的 string 組合都一個一個拿來判定 string item 反轉後是否也與原 string 相同
+                var sSub = s.Substring(i, j);
+                var charArray = sSub.ToCharArray();
+                Array.Reverse(charArray);
+                if (new string(charArray) == sSub)
+                {
+                    if (longest.Length < sSub.Length) longest = sSub; // 更長的 string 就指定至 longest
+                }
             }
         }
 
