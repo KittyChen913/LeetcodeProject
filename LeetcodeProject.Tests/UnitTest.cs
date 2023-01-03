d40a8914c8635e3005e55c317d9ffb7c0d1fa0d0
KittyChen913 <nightmare.kitty111@gmail.com>
2023-01-03 21:12:32 +0800

    ✨ add #55 Jump Game
    final success version

--- LeetcodeProject.Tests/UnitTest.cs
+++ LeetcodeProject.Tests/UnitTest.cs
@@ -101,6 +101,10 @@ public class Tests
 
     [TestCase(new int[] { 2, 3, 1, 1, 4 }, true)]
     [TestCase(new int[] { 3, 2, 1, 0, 4 }, false)]
+    [TestCase(new int[] { 1 }, true)]
+    [TestCase(new int[] { 2, 0 }, true)]
+    [TestCase(new int[] { 2, 5, 0, 0 }, true)]
+    [TestCase(new int[] { 8, 2, 4, 4, 4, 9, 5, 2, 5, 8, 8, 0, 8, 6, 9, 1, 1, 6, 3, 5, 1, 2, 6, 6, 0, 4, 8, 6, 0, 3, 2, 8, 7, 6, 5, 1, 7, 0, 3, 4, 8, 3, 5, 9, 0, 4, 0, 1, 0, 5, 9, 2, 0, 7, 0, 2, 1, 0, 8, 2, 5, 1, 2, 3, 9, 7, 4, 7, 0, 0, 1, 8, 5, 6, 7, 5, 1, 9, 9, 3, 5, 0, 7, 5 }, true)]
     public void CanJump_Success(int[] nums, bool expected)
     {
         var result = JumpGame.CanJump(nums);
--- LeetcodeProject/JumpGame.cs
+++ LeetcodeProject/JumpGame.cs
@@ -2,9 +2,55 @@
 {
     public class JumpGame
     {
-        public static bool CanJump(int[] nums)
+        /// <summary>
+        /// 55. Jump Game
+        /// 遞迴，不斷回頭重 try 嘗試其他可能性
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <param name="startIndex"></param>
+        /// <param name="maxIndex"></param>
+        /// <returns></returns>
+        public static bool CanJump(int[] nums, int startIndex = 0, int maxIndex = 0)
         {
-            return false;
+            // startIndex 是用於紀錄每次重跑的項目位置
+            // jumpIndex 為目前跳到哪一個項目
+            // maxIndex 是紀錄最長的紀錄是跑到哪一個項目位置，用於在下面判定為 0 的項目是否已經是最後的項目了
+
+            int jumpIndex = startIndex;
+
+            // 如果只有 1 個項目，就直接 return 了
+            if (nums.Length == 1) return true;
+
+            while (jumpIndex < nums.Length - 1)
+            {
+                // 無法再繼續走下去了的條件
+                if (nums[jumpIndex].Equals(0) && jumpIndex == startIndex && jumpIndex >= maxIndex)
+                    return false;
+
+                // 如果停在 0 的項目，並且不是最後 1 個項目
+                // 要將這個原本為 false 的結果，遞迴回去繼續做其他可能的嘗試
+                if (nums[jumpIndex].Equals(0) && jumpIndex != nums.Length - 1)
+                {
+                    // 如果起始的 Index 大於 1，代表他還有可以減少步數的空間
+                    // ex: startIndex = 7，只是代表他最多可以走 7 步，但是他也可以走 5 步就好，或 4 步 或 3 步 以此類推.....
+                    if (nums[startIndex] > 1)
+                    {
+                        nums[startIndex] = nums[startIndex] - 1; // 所以逐一遞減它往前走的步數
+                    }
+                    else
+                    {
+                        startIndex++;
+                    }
+                    maxIndex = Math.Max(maxIndex, jumpIndex);
+                    return CanJump(nums, startIndex, maxIndex);
+                }
+
+                // 為了怕超出邊界，最多就 length -1 
+                jumpIndex = (jumpIndex + nums[jumpIndex] > nums.Length - 1) ? nums.Length - 1 : jumpIndex + nums[jumpIndex];
+            }
+
+            // 如果索引並不是停在最後一個項目，就回傳 false，反之如果他成功走到最後一個項目，就回傳 true
+            return (!jumpIndex.Equals(nums.Length - 1)) ? false : true;
         }
     }
 }
