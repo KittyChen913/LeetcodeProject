3fc3b52959e4e59317adf956a1b9a2c6a250317a
KittyChen913 <nightmare.kitty111@gmail.com>
2023-01-05 11:46:03 +0800

    ✨ add #55 Jump Game _ 線性版

--- LeetcodeProject.Tests/UnitTest.cs
+++ LeetcodeProject.Tests/UnitTest.cs
@@ -105,6 +105,8 @@ public class Tests
     [TestCase(new int[] { 2, 0 }, true)]
     [TestCase(new int[] { 2, 5, 0, 0 }, true)]
     [TestCase(new int[] { 8, 2, 4, 4, 4, 9, 5, 2, 5, 8, 8, 0, 8, 6, 9, 1, 1, 6, 3, 5, 1, 2, 6, 6, 0, 4, 8, 6, 0, 3, 2, 8, 7, 6, 5, 1, 7, 0, 3, 4, 8, 3, 5, 9, 0, 4, 0, 1, 0, 5, 9, 2, 0, 7, 0, 2, 1, 0, 8, 2, 5, 1, 2, 3, 9, 7, 4, 7, 0, 0, 1, 8, 5, 6, 7, 5, 1, 9, 9, 3, 5, 0, 7, 5 }, true)]
+    [TestCase(new int[] { 0, 2, 3 }, false)]
+    [TestCase(new int[] { 1, 0, 1, 0 }, false)]
     public void CanJump_Success(int[] nums, bool expected)
     {
         var result = JumpGame.CanJump(nums);
--- LeetcodeProject/JumpGame.cs
+++ LeetcodeProject/JumpGame.cs
@@ -4,13 +4,49 @@
     {
         /// <summary>
         /// 55. Jump Game
+        /// 線性版，逐一往前判定的寫法，處理速度較快
+        /// </summary>
+        /// <param name="nums"></param>
+        /// <returns></returns>
+        public static bool CanJump(int[] nums)
+        {
+            // 如果只有 1 個項目，就直接 return 了
+            if (nums.Length == 1) return true;
+
+            // 從後面開始依序往前判定
+            // 先以最後一個項目的 Index 開始
+            var lastIndex = nums.Length - 1;
+
+            // 之所以 i = lastIndex - 1 是因為迴圈直接從倒數第二個 Index 開始跟 lastIndex 比較
+            for (int i = lastIndex - 1; i >= 0; i--)
+            {
+                // i = 當前 Index
+                // nums[i] = 可以跳的 Index 步數
+                // 判定 (當前 Index) + (可以跳的 Index 步數) 加起來可以走的到 last 的 Index
+                if (i + nums[i] >= lastIndex)
+                {
+                    lastIndex = i;
+                    // 就把 last 的 Index 更新為當前 Index，繼續往前判定
+                }
+                // 如果 (當前 Index) + (可以跳的 Index 步數) 加起來 "走不到" last 的 Index，就不更新 lastIndex，繼續往前找其他 Index 有沒有人可以走到 last 的 Index
+            }
+
+            // ex: 第 3 個如果可以走到 第 4 個，那就再往前判定，第 2 個能否走到第 3 個
+
+            // 如果可以一路判定到最前面(index = 0)，就代表可以一路跳完 return true，反之則 false
+            return lastIndex == 0;
+        }
+
+        /// <summary>
+        /// 55. Jump Game
         /// 遞迴，不斷回頭重 try 嘗試其他可能性
+        /// 但是迴圈版的時間複雜度較高，處理速度慢
         /// </summary>
         /// <param name="nums"></param>
         /// <param name="startIndex"></param>
         /// <param name="maxIndex"></param>
         /// <returns></returns>
-        public static bool CanJump(int[] nums, int startIndex = 0, int maxIndex = 0)
+        public static bool CanJump_Recursion(int[] nums, int startIndex = 0, int maxIndex = 0)
         {
             // startIndex 是用於紀錄每次重跑的項目位置
             // jumpIndex 為目前跳到哪一個項目
@@ -49,7 +85,7 @@
                             startIndex++;
                         }
                         maxIndex = Math.Max(maxIndex, jumpIndex);
-                        return CanJump(nums, startIndex, maxIndex);
+                        return CanJump_Recursion(nums, startIndex, maxIndex);
                     }
                 }
                 jumpIndex += nums[jumpIndex];
